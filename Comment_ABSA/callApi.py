3e7cef89e20d1372b94f722ca1abe6bb586344cc
Mahdi Rahbar <30546576+MahdiRahbar@users.noreply.github.com>
2020-01-14 12:35:18 +0330

    added callApi function

--- Comment_ABSA/callApi.py
+++ Comment_ABSA/callApi.py
@@ -5,3 +5,11 @@ Created on Tue Jan 14 12:30:32 2020
 @author: Mahdi Rahbar
 """
 
+def callApi(url, data, tokenKey):
+    headers = {
+        'Content-Type': "application/json",
+        'Authorization': "Bearer " + tokenKey,
+        'Cache-Control': "no-cache"
+    }
+    response = requests.request("POST", url, data=data.encode("utf-8"), headers=headers)
+    return response.text
