b6c18357b24a89d3a980e71d6860e58f48616646
Mahdi Rahbar <30546576+MahdiRahbar@users.noreply.github.com>
2020-01-16 01:11:14 +0330

    debugged the code

--- Comment_ABSA/DataGenerator.py
+++ Comment_ABSA/DataGenerator.py
@@ -9,6 +9,7 @@ import pandas as pd
 from callApi import *
 import json
 import ast 
+import time
 
 #---------------------------------------------------------------
 api_key = "cbb6763d-3535-ea11-80eb-98ded002619b"
@@ -38,6 +39,10 @@ class DataGenerator:
     
     def Data_Import(self):
         self.input_data = pd.read_csv(self.data_path)
+        # self.input_data =self.input_data[65:75]
+        # self.input_data =self.input_data[164:168]
+        self.input_data =self.input_data[3001:4001]
+
         self.data_length = len(self.input_data)
 
     def Comment_Cleaner(self, str_data):
@@ -141,47 +146,55 @@ class DataGenerator:
         temp = []
 
         final_temp = []
-        internal_list_temp = ast.literal_eval(input_data)
-        # internal_list_temp = input_data
-        for j in range(len(internal_list_temp)):
-                #####################      Text Normalizer       #########################
-            try:
-                baseUrl = "http://api.text-mining.ir/api/"
-                url =  baseUrl + "PreProcessing/NormalizePersianWord"
-                payload = u'{{\"text\":\"{}\", \"refineSeparatedAffix\":true}}'.format(internal_list_temp[j])
+        try:
+            internal_list_temp = ast.literal_eval(input_data)
+            # internal_list_temp = input_data
+            for j in range(len(internal_list_temp)):
+                    #####################      Text Normalizer       #########################
+                if internal_list_temp[j] != '' and internal_list_temp[j] != "\r":
+                    # print("yay")
+                    try:
+                        baseUrl = "http://api.text-mining.ir/api/"
+                        url =  baseUrl + "PreProcessing/NormalizePersianWord"
+                        payload = u'{{\"text\":\"{}\", \"refineSeparatedAffix\":true}}'.format(internal_list_temp[j])
 
 
-                temp=callApi(url, payload, tokenKey)
-            except Exception as e: 
-                print("The Error is : ", e)
-                print("Error on iteration: {0} function: {1} ".format(i,"Text Normalizer"))
-                self.ErrorListIndex.append(i)
-                continue
+                        temp=callApi(url, payload, tokenKey)
+                    except Exception as e: 
+                        print("The Error is : ", e)
+                        print("Error on iteration: {0} function: {1} ".format(i,"Text Normalizer"))
+                        self.ErrorListIndex.append(i)
+                        continue
 
 
-    #         ################ Sentence Tokenizer ##################
-            try:
-                url =  baseUrl + "PreProcessing/SentenceSplitter"
-                payload = u'''{\"text\":\"%s\",
-                        \"checkSlang\": true,
-                        \"normalize\": true,
-                        \"normalizerParams\": {
-                        \"text\": \"don't care\",
-                        \"RefineQuotationPunc \": false
-                        },
-                        \"complexSentence\": true
-                        }'''%temp
-                temp = callApi(url, payload, tokenKey)
-                temp = ast.literal_eval(temp)
+            #         ################ Sentence Tokenizer ##################
+                    try:
+                        url =  baseUrl + "PreProcessing/SentenceSplitter"
+                        payload = u'''{\"text\":\"%s\",
+                                \"checkSlang\": true,
+                                \"normalize\": true,
+                                \"normalizerParams\": {
+                                \"text\": \"don't care\",
+                                \"RefineQuotationPunc \": false
+                                },
+                                \"complexSentence\": true
+                                }'''%temp
+                        temp = callApi(url, payload, tokenKey)
+                        temp = ast.literal_eval(temp)
 
-            except Exception as e: 
-                print("The Error is : ", e)
-                print("Error on iteration: {0} function: {1} ".format(i,"Sentence Tokenizer"))
-                self.ErrorListIndex.append(i)
+                    except Exception as e: 
+                        print("The Error is : ", e)
+                        print("Error on iteration: {0} function: {1} ".format(i,"Sentence Tokenizer"))
+                        self.ErrorListIndex.append(i)
 
-                continue
-            for l in range(len(temp)):
-                final_temp.append(temp[l])
+                        continue
+                    for l in range(len(temp)):
+                        final_temp.append(temp[l])
+        except Exception as e:
+            print("The Error is : ", e)
+            print("Error on iteration: {0} function: {1} ".format(i,"Adv_Disadv "))
+            self.ErrorListIndex.append(i)
+            final_temp.append('')
         return final_temp
     
     def Adv_Disadv_2_Formal(self, input_data):
@@ -192,85 +205,99 @@ class DataGenerator:
         temp = []
 
         final_temp = []
-        internal_list_temp = ast.literal_eval(input_data)
-        for j in range(len(internal_list_temp)):
-                #####################      Text Normalizer       #########################
-            try:
-                baseUrl = "http://api.text-mining.ir/api/"
-                url =  baseUrl + "PreProcessing/NormalizePersianWord"
-                payload = u'{{\"text\":\"{}\", \"refineSeparatedAffix\":true}}'.format(internal_list_temp[j])
+        try:
+            internal_list_temp = ast.literal_eval(input_data)
+            for j in range(len(internal_list_temp)):
+                    #####################      Text Normalizer       #########################
+                try:
+                    baseUrl = "http://api.text-mining.ir/api/"
+                    url =  baseUrl + "PreProcessing/NormalizePersianWord"
+                    payload = u'{{\"text\":\"{}\", \"refineSeparatedAffix\":true}}'.format(internal_list_temp[j])
 
 
-                temp=callApi(url, payload, tokenKey)
-            except Exception as e: 
-                print("The Error is : ", e)
-                print("Error on iteration: {0} function: {1} ".format(i,"Text Normalizer"))
-                self.ErrorListIndex.append(i)
-                continue
+                    temp=callApi(url, payload, tokenKey)
+                except Exception as e: 
+                    print("The Error is : ", e)
+                    print("Error on iteration: {0} function: {1} ".format(i,"Text Normalizer"))
+                    self.ErrorListIndex.append(i)
+                    continue
 
-    #         ################ Call Slang to Formal Converter ##################
-            try:
-                url =  baseUrl + "TextRefinement/FormalConverter"
-                payload = u'\"{0}\"'.format(temp)
-                temp = callApi(url, payload, tokenKey)
-            except Exception as e: 
-                print("The Error is : ", e)
-                print("Error on iteration: {0} function: {1} ".format(i,"Slang to Formal Converter"))
-                self.ErrorListIndex.append(i)
+        #         ################ Call Slang to Formal Converter ##################
+                try:
+                    url =  baseUrl + "TextRefinement/FormalConverter"
+                    payload = u'\"{0}\"'.format(temp)
+                    temp = callApi(url, payload, tokenKey)
+                except Exception as e: 
+                    print("The Error is : ", e)
+                    print("Error on iteration: {0} function: {1} ".format(i,"Slang to Formal Converter"))
+                    self.ErrorListIndex.append(i)
 
-                continue
-    #         ################ Sentence Tokenizer ##################
-            try:
-                url =  baseUrl + "PreProcessing/SentenceSplitter"
-                payload = u'''{\"text\":\"%s\",
-                        \"checkSlang\": true,
-                        \"normalize\": true,
-                        \"normalizerParams\": {
-                        \"text\": \"don't care\",
-                        \"RefineQuotationPunc \": false
-                        },
-                        \"complexSentence\": true
-                        }'''%temp
-                temp = callApi(url, payload, tokenKey)
-                temp = ast.literal_eval(temp)
+                    continue
+        #         ################ Sentence Tokenizer ##################
+                try:
+                    url =  baseUrl + "PreProcessing/SentenceSplitter"
+                    payload = u'''{\"text\":\"%s\",
+                            \"checkSlang\": true,
+                            \"normalize\": true,
+                            \"normalizerParams\": {
+                            \"text\": \"don't care\",
+                            \"RefineQuotationPunc \": false
+                            },
+                            \"complexSentence\": true
+                            }'''%temp
+                    temp = callApi(url, payload, tokenKey)
+                    temp = ast.literal_eval(temp)
 
-            except Exception as e: 
-                print("The Error is : ", e)
-                print("Error on iteration: {0} function: {1} ".format(i,"Sentence Tokenizer"))
-                self.ErrorListIndex.append(i)
+                except Exception as e: 
+                    print("The Error is : ", e)
+                    print("Error on iteration: {0} function: {1} ".format(i,"Sentence Tokenizer"))
+                    self.ErrorListIndex.append(i)
 
-                continue
-            for l in range(len(temp)):
-                final_temp.append(temp[l])
-        return final_temp
+                    continue
+                for l in range(len(temp)):
+                    final_temp.append(temp[l])
+            return final_temp
+        except Exception as e: 
+            print("The Error is : ", e)
+            print("Error on iteration: {0} function: {1} ".format(i,"Slang to Formal Converter"))
+            self.ErrorListIndex.append(i)
 
     def PoS_Extractor(self, input_data):
         """
         
         """
-        print(input_data)
-        print(type(input_data))
-        url =  baseUrl + "PosTagger/GetPos"
-        temp_list = [] 
-        new_list = list(input_data)
-        for i in range(len(new_list)):
+        # print(input_data)
+        # print(type(input_data))
+        try:
+            url =  baseUrl + "PosTagger/GetPos"
+            temp_list = [] 
+            new_list = list(input_data)
+            for i in range(len(new_list)):
 
-            sentence_POS = []
-            # print(new_list[i])
-            payload = u'\"{0}\"'.format(new_list[i])                
-            result = json.loads(callApi(url, payload, tokenKey))
-            for phrase in result:
-                sentence_POS.append("("+phrase['word']+","+phrase['tags']['POS']['item1']+")")
+                sentence_POS = []
+                # print(new_list[i])
+                payload = u'\"{0}\"'.format(new_list[i])                
+                result = json.loads(callApi(url, payload, tokenKey))
+                for phrase in result:
+                    sentence_POS.append("("+phrase['word']+","+phrase['tags']['POS']['item1']+")")
 
-            temp_list.append(sentence_POS)
+                temp_list.append(sentence_POS)
+        except Exception as e: 
+            print("The Error is : ", e)
+            print("Error on iteration: {0} function: {1} ".format(self.counter,"PoS extractor"))
+            self.ErrorListIndex.append(i)
+
         return temp_list
 
     def Data_cleaner(self):
         temp_data = []
-        self.data_length = 3
+        # self.data_length = 1001
 
         for i in range(self.data_length):
-            print("Hello from the other sideeee!")
+            print("--------------------------------------")
+            start_temp_time = time.time()
+            print("Record %d is in progress..."%(i+1))
+            # print("Hello from the other sideeee!")
             self.counter = i
             temp_data = []
             temp_data.append(self.input_data.iloc[i]["product_title"])
@@ -304,13 +331,13 @@ class DataGenerator:
             
             temp_data.append(list(temp_DisAdv))
             temp_data.append(list(self.PoS_Extractor(temp_DisAdv)))
-            print("=====================================")
-            print(temp_data)
-            print(self.output_data)
+            # print("=====================================")
+            # print(temp_data)
+            # print(self.output_data)
             self.output_data.append(list(temp_data))
-            print("-------------------------------------")
-
-            print(self.output_data)
+            # print("-------------------------------------")
+            print("total time for record {:d} is: {:.2f}s".format((i+1),time.time()-start_temp_time))
+            # print(self.output_data)
         return self.output_data , self.ErrorListIndex
 
 
