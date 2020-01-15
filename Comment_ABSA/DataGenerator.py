7b253c18719c85541557447ce6e9edb76081534c
Mahdi Rahbar <30546576+MahdiRahbar@users.noreply.github.com>
2020-01-15 13:13:11 +0330

    Update DataGenerator.py

--- Comment_ABSA/DataGenerator.py
+++ Comment_ABSA/DataGenerator.py
@@ -142,6 +142,7 @@ class DataGenerator:
 
         final_temp = []
         internal_list_temp = ast.literal_eval(input_data)
+        # internal_list_temp = input_data
         for j in range(len(internal_list_temp)):
                 #####################      Text Normalizer       #########################
             try:
@@ -247,22 +248,29 @@ class DataGenerator:
         """
         
         """
+        print(input_data)
+        print(type(input_data))
         url =  baseUrl + "PosTagger/GetPos"
         temp_list = [] 
         new_list = list(input_data)
         for i in range(len(new_list)):
+
             sentence_POS = []
+            # print(new_list[i])
             payload = u'\"{0}\"'.format(new_list[i])                
             result = json.loads(callApi(url, payload, tokenKey))
             for phrase in result:
                 sentence_POS.append("("+phrase['word']+","+phrase['tags']['POS']['item1']+")")
+
             temp_list.append(sentence_POS)
         return temp_list
 
     def Data_cleaner(self):
-        for i in range(self.data_length):
-            temp_data = pd.DataFrame(self.input_data)
+        temp_data = []
+        self.data_length = 1
 
+        for i in range(self.data_length):
+            print("Hello from the other sideeee!")
             self.counter = i
             temp_data = []
             temp_data.append(self.input_data.iloc[i]["product_title"])
@@ -272,33 +280,89 @@ class DataGenerator:
             ### Comment Section 
             temp_data.append(self.input_data.iloc[i]["comment"])
             # Cleaning Comment
-            Cleaned_Comment_Output = self.Comment_Cleaner(self.input_data.iloc[i]["comment"])
-            temp_data.append(Cleaned_Comment_Output)
-            temp_data.append(self.PoS_Extractor(Cleaned_Comment_Output))
+
+            # Cleaned_Comment_Output = self.Comment_Cleaner(self.input_data.iloc[i]["comment"])
+            Cleaned_Comment_Output = ast.literal_eval(self.Comment_Cleaner(self.input_data.iloc[i]["comment"]))
+            temp_data.append(list(Cleaned_Comment_Output))
+            # print(Cleaned_Comment_Output)
+            # print(type(Cleaned_Comment_Output))
+            temp_data.append(list(self.PoS_Extractor(Cleaned_Comment_Output)))
         #===========================================================================
             ### Advantages Section 
             temp_data.append(self.input_data.iloc[i]["advantages"])
             # Cleaning Advantages
             Cleaned_Adv = self.Adv_Disadv(self.input_data.iloc[i]["advantages"])
-            temp_data.append(Cleaned_Adv)
-            temp_data.append(self.PoS_Extractor(Cleaned_Adv))
+            # Cleaned_Adv = self.Adv_Disadv(ast.literal_eval(self.Adv_Disadv(self.input_data.iloc[i]["advantages"])))
+            temp_data.append(list(Cleaned_Adv))
+            temp_data.append(list(self.PoS_Extractor(Cleaned_Adv)))
         #===========================================================================
             ### Disadvantages Section 
             temp_data.append(self.input_data.iloc[i]["disadvantages"])
             # Cleaning Disadvantages
             temp_DisAdv = self.Adv_Disadv(self.input_data.iloc[i]["disadvantages"])
-            temp_data.append(temp_DisAdv)
-            temp_data.append(self.PoS_Extractor(self.input_data.iloc[i]["disadvantages"]))
+            # temp_DisAdv = ast.literal_eval(self.Adv_Disadv(self.input_data.iloc[i]["disadvantages"]))
             
-            self.output_data.append(temp_data)
+            temp_data.append(list(temp_DisAdv))
+            temp_data.append(list(self.PoS_Extractor(temp_DisAdv)))
+            print("=====================================")
+            print(temp_data)
+            print(self.output_data)
+            self.output_data.append(list(temp_data))
+            print("-------------------------------------")
+
+            print(self.output_data)
         return self.output_data , self.ErrorListIndex
 
+
+    # def Data_cleaner(self):
+    #     temp_data = []
+    #     self.data_length = 2 
+
+    #     for i in range(self.data_length):
+    #         print("Hello from the other sideeee!")
+    #         self.counter = i
+    #         temp_data = {"product_title":[],"recommend":[],"title":[],"comment":[],"Comment_Sentence_List":[],"Comment_PoS":[],
+    #                     "Advantage":[],"Advantage_Normalizer":[],"Advantage_Sentences":[],"Disadvantage":[],
+    #                     "Disadvantage_Normalizer":[],"Disadvantage_Sentences":[]}
+    #         temp_data["product_title"].append(self.input_data.iloc[i]["product_title"])
+    #         temp_data["recommend"].append(self.input_data.iloc[i]["recommend"])
+    #         temp_data["title"].append(self.input_data.iloc[i]["title"])
+    #     #===========================================================================
+    #         ### Comment Section 
+    #         temp_data["comment"].append(self.input_data.iloc[i]["comment"])
+    #         # Cleaning Comment
+
+    #         # Cleaned_Comment_Output = self.Comment_Cleaner(self.input_data.iloc[i]["comment"])
+    #         Cleaned_Comment_Output = ast.literal_eval(self.Comment_Cleaner(self.input_data.iloc[i]["comment"]))
+    #         temp_data["Comment_Sentence_List"].append(list(Cleaned_Comment_Output))
+    #         # print(Cleaned_Comment_Output)
+    #         # print(type(Cleaned_Comment_Output))
+    #         temp_data["Comment_PoS"].append(list(self.PoS_Extractor(Cleaned_Comment_Output)))
+    #     #===========================================================================
+    #         ### Advantages Section 
+    #         temp_data["Advantage"].append(self.input_data.iloc[i]["advantages"])
+    #         # Cleaning Advantages
+    #         Cleaned_Adv = self.Adv_Disadv(self.input_data.iloc[i]["advantages"])
+    #         # Cleaned_Adv = self.Adv_Disadv(ast.literal_eval(self.Adv_Disadv(self.input_data.iloc[i]["advantages"])))
+    #         temp_data["Advantage_Normalizer"].append(list(Cleaned_Adv))
+    #         temp_data["Advantage_Sentences"].append(list(self.PoS_Extractor(Cleaned_Adv)))
+    #     #===========================================================================
+    #         ### Disadvantages Section 
+    #         temp_data["Disadvantage"].append(self.input_data.iloc[i]["disadvantages"])
+    #         # Cleaning Disadvantages
+    #         temp_DisAdv = self.Adv_Disadv(self.input_data.iloc[i]["disadvantages"])
+    #         # temp_DisAdv = ast.literal_eval(self.Adv_Disadv(self.input_data.iloc[i]["disadvantages"]))
+            
+    #         temp_data["Disadvantage_Normalizer"].append(list(temp_DisAdv))
+    #         temp_data["Disadvantage_Sentences"].append(list(self.PoS_Extractor(temp_DisAdv)))
+            
+    #         self.output_data.append(list(temp_data))
+
+    #     return self.output_data , self.ErrorListIndex
 
 
 
     def Data_cleaner_2_formal(self):
-        temp_data = pd.DataFrame(self.input_data)
-
 
         for i in range(self.data_length):
             self.counter = i
@@ -311,23 +375,30 @@ class DataGenerator:
             temp_data.append(self.input_data.iloc[i]["comment"])
             # Cleaning Comment
             Cleaned_Comment = self.Comment_Cleaner(self.input_data.iloc[i]["comment"])
-            temp_data.append(Cleaned_Comment)
-            temp_data.append(self.PoS_Extractor(Cleaned_Comment))
+            temp_data.append(list(Cleaned_Comment))
+            temp_data.append(list(self.PoS_Extractor(Cleaned_Comment)))
         #===========================================================================
             ### Advantages Section 
             temp_data.append(self.input_data.iloc[i]["advantages"])
             # Cleaning Advantages
             Cleaned_Adv = self.Adv_Disadv_2_Formal(self.input_data.iloc[i]["advantages"])
-            temp_data.append(Cleaned_Adv)
-            temp_data.append(self.PoS_Extractor(self.input_data.iloc[i]["advantages"]))
+            temp_data.append(list(Cleaned_Adv))
+            temp_data.append(list(self.PoS_Extractor(self.input_data.iloc[i]["advantages"])))
         #===========================================================================
             ### Disadvantages Section 
             temp_data.append(self.input_data.iloc[i]["disadvantages"])
             # Cleaning Disadvantages
             temp_DisAdv = self.Adv_Disadv_2_Formal(self.input_data.iloc[i]["disadvantages"])
-            temp_data.append(temp_DisAdv)
+            temp_data.append(list(temp_DisAdv))
             temp_data.append(self.PoS_Extractor(temp_DisAdv))
             
-            self.output_data_2_formal.append(temp_data)
+            self.output_data_2_formal.append(list(temp_data))
 
         return self.output_data_2_formal , self.ErrorListIndex_formal
+
+    def Save_Dataset(self, data_list):
+        temp_dataframe = pd.DataFrame(data_list,index=None)
+        # print(temp_dataframe.head(3))
+        temp_dataframe.to_csv(self.save_path,index=None)  # ,header=["product_title","recommend","title","comment","Comment_Sentence_List","Comment_PoS",
+                                                          #         "Advantage","Advantage_Normalizer","Advantage_Sentences","Disadvantage",
+                                                          #         "Disadvantage_Normalizer","Disadvantage_Sentences"]
