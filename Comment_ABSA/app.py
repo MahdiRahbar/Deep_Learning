d87c447f9d7bac4445696ef5e304da47feb82cfa
Mahdi Rahbar <30546576+MahdiRahbar@users.noreply.github.com>
2020-01-15 15:49:34 +0330

    Update app.py

--- Comment_ABSA/app.py
+++ Comment_ABSA/app.py
@@ -7,5 +7,5 @@ def main():
     NewDataset = DataGenerator("./dataset/comments_cleaned_1st_phase.csv","./dataset/test.csv")  # This line doesn't have a valid save_path
 
     NewDataset.Data_Import()
-    saved_data=NewDataset.Data_cleaner()
-    NewDataset.Save_Dataset(saved_data)
+    NewDataset.Data_cleaner()
+    NewDataset.Save_Dataset()
