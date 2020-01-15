d64a668d61da1ee8c2376e864a977b96da102905
Mahdi Rahbar <30546576+MahdiRahbar@users.noreply.github.com>
2020-01-16 01:11:01 +0330

    Updated the main function, debugged the code and added time log

--- Comment_ABSA/app.py
+++ Comment_ABSA/app.py
@@ -1,11 +1,13 @@
 from DataGenerator import DataGenerator 
 import pandas as pd 
 import numpy as np 
-
+import time
 
 def main():
-    NewDataset = DataGenerator("./dataset/comments_cleaned_1st_phase.csv","./dataset/test.csv")  # This line doesn't have a valid save_path
-
+    NewDataset = DataGenerator("./dataset/comments_cleaned_1st_phase.csv","./dataset/digi_cleaned_phase2_3000_4000.csv")  # This line doesn't have a valid save_path
+    start_time = time.time()
     NewDataset.Data_Import()
     NewDataset.Data_cleaner()
     NewDataset.Save_Dataset()
+    print("============================")
+    print("Finished in %.2f s"%(time.time()-start_time))
