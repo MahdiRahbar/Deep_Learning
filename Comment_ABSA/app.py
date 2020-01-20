5712e5bcf7f79a3aff9f399f08ca932660cca32b
Mahdi Rahbar <30546576+MahdiRahbar@users.noreply.github.com>
2020-01-20 13:11:27 +0330

    Update app.py

--- Comment_ABSA/app.py
+++ Comment_ABSA/app.py
@@ -4,7 +4,7 @@ import numpy as np
 import time
 
 def main():
-    NewDataset = DataGenerator("./dataset/comments_cleaned_1st_phase.csv","./dataset/digi_cleaned_phase2_3000_4000.csv")  # This line doesn't have a valid save_path
+    NewDataset = DataGenerator("./dataset/comments_cleaned_1st_phase.csv","./dataset/digi_cleaned_phase2.csv")  # This line doesn't have a valid save_path
     start_time = time.time()
     NewDataset.Data_Import()
     NewDataset.Data_cleaner()
