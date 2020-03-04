0562393fdd0e6a3e888f66451567005694d5ee62
mahdirahbar <MahdiRahbar01@gmail.com>
2020-03-04 13:23:26 +0330

    simplified data_sepfunction to seperate labeled data

--- HAR/data_prep.py
+++ HAR/data_prep.py
@@ -21,28 +21,12 @@ def data_read(path):
 def data_sep(path, start_index, end_index):
     data = data_read(path)
     data = data.drop(['Index','Arrival_Time','Creation_Time','User','Model','Device'], axis=1)
-    d0 = data[data['labels']==0]
-    d1 = data[data['labels']==1]
-    d2 = data[data['labels']==2]
-    d3 = data[data['labels']==3]
-    d4 = data[data['labels']==4]
-    d5 = data[data['labels']==5]
-    #####============================
-    d0_arr = np.reshape(np.array(d0[['x','y','z']]),(len(d0),3))
-    # ====================================================================================================
-    d1_arr = np.reshape(np.array(d1[['x','y','z']]),(len(d1),3))
-    # ====================================================================================================
-    d2_arr = np.reshape(np.array(d2[['x','y','z']]),(len(d2),3))
-    # ====================================================================================================
-    d3_arr = np.reshape(np.array(d3[['x','y','z']]),(len(d3),3))
-    # ====================================================================================================
-    d4_arr = np.reshape(np.array(d4[['x','y','z']]),(len(d4),3))
-    # ====================================================================================================
-    d5_arr = np.reshape(np.array(d5[['x','y','z']]),(len(d5),3))
-    #####============================
-    train_data = [d0_arr[start_index:end_index],d1_arr[start_index:end_index],d2_arr[start_index:end_index],
-                  d3_arr[start_index:end_index],d4_arr[start_index:end_index],d5_arr[start_index:end_index]]
-    return train_data
+    final_data = []
+    for i in range(6): # number of classes
+        temp = data[data['labels']==i]
+        temp = np.reshape(np.array(temp[['x','y','z']]),(len(temp),3))
+        final_data.append(temp[start_index:end_index])
+    return final_data
 
 class MultiProcessPreprocessor:
     """
