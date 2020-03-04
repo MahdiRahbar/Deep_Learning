0051b789e4ad7b0ad74862600c8160f9d362a925
mahdirahbar <MahdiRahbar01@gmail.com>
2020-03-04 12:25:54 +0330

    added multiprocess preprocessor to manage the sequential data windowing

--- HAR/data_prep.py
+++ HAR/data_prep.py
@@ -7,6 +7,9 @@ Created on Mon Mar  2 12:59:10 2020
 """
 import pandas as pd
 import numpy as np
+from multiprocessing import Process, Lock
+from multiprocessing.sharedctypes import Array
+import multiprocessing
 
 def Data_Read(path):
     data = pd.read_csv(path)
@@ -39,3 +42,24 @@ def Data_Sep(path, start_index, end_index):
     train_data = [d0_arr[start_index:end_index],d1_arr[start_index:end_index],d2_arr[start_index:end_index],
                   d3_arr[start_index:end_index],d4_arr[start_index:end_index],d5_arr[start_index:end_index]]
     return train_data
+
+
+def MP_Preprocessor(data, window_size):
+    Bi_labels = [BitArray([0, 0, 0, 0, 0, 1]), BitArray([0, 0, 0, 0, 1, 0]), BitArray([0, 0, 0, 1, 0, 0]),
+                 BitArray([0, 0, 1, 0, 0, 0]), \
+                 BitArray([0, 1, 0, 0, 0, 0]), BitArray([1, 0, 0, 0, 0, 0])]
+    print("Dataset preprocessing in progress...")
+    init_time = time.time()
+    lock = Lock()
+    #     array = Array('i', 10000, lock=lock)
+    X, Y = np.empty((0, window_size, 3)), np.empty((0, 6))
+
+    for i in range(len(data)):
+        #         p = multiprocessing.Pool(processes = multiprocessing.cpu_count()-1)
+        p = Process(target=data_prep, args=(i, data, X, Y, Bi_labels))
+        p.start()
+        p.join()
+    X = np.reshape(X, (len(X), window_size, 3))
+    Y = np.reshape(Y, (len(Y), 6))
+    print("Dataset was preprocessed in: %.2fs" % (time.time() - init_time))
+    return X, Y
