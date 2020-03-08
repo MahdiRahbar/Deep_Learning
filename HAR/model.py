80636d6f64cef69b461deaa452fbc15c1ba65ea8
mahdirahbar <MahdiRahbar01@gmail.com>
2020-03-08 23:45:58 +0330

    updated required header libraries for model.py

--- HAR/model.py
+++ HAR/model.py
@@ -5,3 +5,9 @@ Created on Mon Mar  2 12:59:10 2020
 
 @author: mahdi
 """
+from keras.layers import LSTM, Input, Dense
+from keras.models import Model
+from keras.utils import to_categorical
+from keras import optimizers
+
+
