9d7d3c28f1be3a751b5d6cf4e2429a4afd09155c
mahdirahbar <MahdiRahbar01@gmail.com>
2020-03-10 14:00:40 +0330

    added the main structure

--- HAR/EA.py
+++ HAR/EA.py
@@ -5,3 +5,34 @@ Created on Mon Mar  2 12:59:10 2020
 
 @author: mahdi
 """
+from deap import base, creator, tools, algorithms
+from scipy.stats import bernoulli
+from bitstring import BitArray
+
+# population_size = 10  # Too big
+# num_generations = 15  # Takes for ever to run
+
+population_size = 4
+num_generations = 8
+gene_length = 14
+
+# As we are trying to minimize the RMSE score, that's why using -1.0. |
+# In case, when you want to maximize accuracy for instance, use 1.0
+creator.create('FitnessMax', base.Fitness, weights=(1.0,))
+creator.create('Individual', list, fitness=creator.FitnessMax)
+
+toolbox = base.Toolbox()
+toolbox.register('binary', bernoulli.rvs, 0.5)
+toolbox.register('individual', tools.initRepeat, creator.Individual, toolbox.binary, n=gene_length)
+toolbox.register('population', tools.initRepeat, list, toolbox.individual)
+
+toolbox.register('mate', tools.cxOrdered)
+toolbox.register('mutate', tools.mutShuffleIndexes, indpb=0.6)
+toolbox.register('select', tools.selRoulette)
+toolbox.register('evaluate', train_evaluate)
+
+start_time = time.time()
+population = toolbox.population(n=population_size)
+r = algorithms.eaSimple(population, toolbox, cxpb=0.4, mutpb=0.1, ngen=num_generations, verbose=False)
+
+print('Finished in %.2f s' % (time.time() - start_time))
