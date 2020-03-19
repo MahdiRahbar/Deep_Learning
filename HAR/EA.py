4cacd82838d0302a16c828c14b5576e76027487f
mahdirahbar <MahdiRahbar01@gmail.com>
2020-03-19 14:07:50 +0330

    updated EA and fixed variables

--- HAR/EA.py
+++ HAR/EA.py
@@ -8,21 +8,19 @@ Created on Mon Mar  2 12:59:10 2020
 from deap import base, creator, tools, algorithms
 from scipy.stats import bernoulli
 from bitstring import BitArray
-import model
+from model import deep_model
+import time
 # population_size = 10  # Too big
 # num_generations = 15  # Takes for ever to run
 
 
 class EA:
 
-    def __init__(self, population_size, num_generations, gene_length, window_size_gen):
+    def __init__(self, population_size, num_generations, gene_length):
         self.population_size = population_size
         self.num_generations = num_generations
         self.gene_length = gene_length
-        self.window_size_gen = window_size_gen
-        self.unit_number_gen = self.gene_length - self.window_size_gen
-        self.split_point = self.window_size_gen - 1
-    SPLIT_POINT = self.split_point
+
 
     def evo_algorithm(self):
         # As we are trying to minimize the RMSE score, that's why using -1.0. |
@@ -32,7 +30,7 @@ class EA:
 
         toolbox = base.Toolbox()
         toolbox.register('binary', bernoulli.rvs, 0.5)
-        toolbox.register('individual', tools.initRepeat, creator.Individual, toolbox.binary, n=gene_length)
+        toolbox.register('individual', tools.initRepeat, creator.Individual, toolbox.binary, n=self.gene_length)
         toolbox.register('population', tools.initRepeat, list, toolbox.individual)
 
         toolbox.register('mate', tools.cxOrdered)
@@ -41,8 +39,8 @@ class EA:
         toolbox.register('evaluate', deep_model)
 
         start_time = time.time()
-        population = toolbox.population(n=population_size)
-        r = algorithms.eaSimple(population, toolbox, cxpb=0.4, mutpb=0.1, ngen=num_generations, verbose=False)
+        population = toolbox.population(n=self.population_size)
+        r = algorithms.eaSimple(population, toolbox, cxpb=0.4, mutpb=0.1, ngen=self.num_generations, verbose=False)
 
         print('Finished in %.2f s' % (time.time() - start_time))
 
@@ -50,11 +48,11 @@ class EA:
         best_window_size = None
         best_num_units = None
 
-        print(BitArray(best_individuals[0][0:6]).uint)
+        print(BitArray(best_individuals[0][0:SPLIT_POINT]).uint)
 
         for bi in best_individuals:
-            window_size_bits = BitArray(bi[0:6])
-            num_units_bits = BitArray(bi[6:])
+            window_size_bits = BitArray(bi[0:SPLIT_POINT])
+            num_units_bits = BitArray(bi[SPLIT_POINT:])
             best_window_size = window_size_bits.uint
             best_num_units = num_units_bits.uint
             print('\nWindow Size: ', best_window_size, ', Num of Units: ', best_num_units)
