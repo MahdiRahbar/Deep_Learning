45ad7dde7f6204eb349523d66b93f039b4bf0103
mahdirahbar <MahdiRahbar01@gmail.com>
2020-03-25 18:47:36 +0430

    changed model variable and delete the made object to help the busy processes get free

--- HAR/model.py
+++ HAR/model.py
@@ -9,13 +9,22 @@ from keras.layers import LSTM, Input, Dense
 from keras.models import Model
 from keras.utils import to_categorical
 from keras import optimizers
+
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split as split
+
 from bitstring import BitArray
+from MultiProcessPreprocessor import MultiProcessPreprocessor
+import time
+from settings import *
 
 
 def deep_model(ga_individual_solution):
     # Decode GA solution to integer for window_size and num_units
     # window_size = self.window_size
     # num_units = self.num_units
+
+    train_data = DATA
     split_point = SPLIT_POINT
 
     window_size_bits = BitArray(ga_individual_solution[:split_point])
@@ -32,7 +41,13 @@ def deep_model(ga_individual_solution):
     if window_size == 0 or num_units == 0:
         return 100,
 
-    X,Y = prepare_dataset(train_data,window_size)
+    print("DEBUG: The data size is : ", len(train_data[0]))
+    mp_prep = MultiProcessPreprocessor(train_data, window_size)
+    X,Y = mp_prep.mp_preprocessor()
+    del mp_prep
+    # X,Y = prepare_dataset(train_data,window_size)
+    print("DEBUG: The X size is : ", len(X))
+
     X_train, X_val, y_train, y_val = split(X, Y, test_size = 0.20, random_state = 1120)
 
     inputs = Input(shape=(window_size, 3))
@@ -44,11 +59,11 @@ def deep_model(ga_individual_solution):
     opt = optimizers.SGD(lr=0.01, momentum=0.9)
     model = Model(inputs=inputs, outputs=predictions)
     model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
-    model.fit(X_trainـMT, y_trainـMT, epochs=5, batch_size=10, shuffle=True)
+    model.fit(X_train, y_train, epochs=3, batch_size=20, shuffle=True)
 
     _, train_acc = model.evaluate(X_train, y_train, verbose=0)
     _, test_acc = model.evaluate(X_val, y_val, verbose=0)
     print('Train: %.3f, Test: %.3f' % (train_acc, test_acc))
 
-    return test_acc,
+    return test_acc, model
 
