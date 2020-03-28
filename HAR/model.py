2d4d1e18ae370f0cfa083b54d5001d7ff32cc33a
mahdirahbar <MahdiRahbar01@gmail.com>
2020-03-29 03:21:00 +0430

    fixed some bugs

--- HAR/model.py
+++ HAR/model.py
@@ -21,7 +21,13 @@ from settings import *
 
 def deep_model(ga_individual_solution):
     # Decode GA solution to integer for window_size and num_units
+    global GEN_COUNTER
+    GEN_COUNTER += 1
 
+    print("=========================================================")
+    print()
+    print("Generation number: %d"%((GEN_COUNTER/GENE_LEN)+1))
+    print("Calculating Chromosome number: %d"%(GEN_COUNTER%GENE_LEN))
 
     train_data = DATA
     split_point = SPLIT_POINT
@@ -38,31 +44,34 @@ def deep_model(ga_individual_solution):
 
     # Return fitness score of 100 if window_size or num_unit is zero
     if window_size == 0 or num_units == 0:
-        return 100,
+        return 0,
 
-    print("DEBUG: The data size is : ", len(train_data[0]))
+    # print("DEBUG: The data size is : ", len(train_data[0]))
     mp_prep = MultiProcessPreprocessor(train_data, window_size)
     X,Y = mp_prep.mp_preprocessor()
     del mp_prep
     # X,Y = prepare_dataset(train_data,window_size)
-    print("DEBUG: The X size is : ", len(X))
+    # print("DEBUG: The X size is : ", len(X))
 
     X_train, X_val, y_train, y_val = split(X, Y, test_size = 0.20, random_state = 1120)
 
     inputs = Input(shape=(window_size, 3))
     x = LSTM(num_units, input_shape=(window_size, 3))(inputs)
     ##
+    x = Dense(200, activation='relu')(x)
+    x = Dense(100, activation='relu')(x)
     x = Dense(50, activation='relu')(x)
     ##
     predictions = Dense(6, activation='softmax')(x)
     opt = optimizers.SGD(lr=0.01, momentum=0.9)
     model = Model(inputs=inputs, outputs=predictions)
     model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
-    model.fit(X_train, y_train, epochs=3, batch_size=20, shuffle=True)
+    model.fit(X_train, y_train, epochs=1, batch_size=20, shuffle=True, validation_data=(X_val, y_val))
 
     _, train_acc = model.evaluate(X_train, y_train, verbose=0)
     _, test_acc = model.evaluate(X_val, y_val, verbose=0)
     print('Train: %.3f, Test: %.3f' % (train_acc, test_acc))
+    del model
 
-    return test_acc
+    return test_acc,
 
